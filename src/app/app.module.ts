0c70cee03d06ffdaf24cfe34f9074378e206003c
@Ash9311 <ashutoshmulki7@gmail.com>
2022-02-16 11:38:12 +0530

    init

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+
+import { AppComponent } from './app.component';
+import { CockPitComponent } from './cock-pit/cock-pit.component';
+import { ServerElementComponent } from './server-element/server-element.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CockPitComponent,
+    ServerElementComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/cock-pit/cock-pit.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cock-pit',
+  templateUrl: './cock-pit.component.html',
+  styleUrls: ['./cock-pit.component.css']
+})
+export class CockPitComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  serverElements = [];
+  newServerName = '';
+  newServerContent = '';
+
+  onAddServer() {
+    this.serverElements.push({
+      type: 'server',
+      name: this.newServerName,
+      content: this.newServerContent
+    });
+  }
+
+  onAddBlueprint() {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: this.newServerName,
+      content: this.newServerContent
+    });
+  }
+}
